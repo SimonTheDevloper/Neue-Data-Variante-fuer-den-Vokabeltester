5168f8380a18610b6d3dba5e0a8fa22edc687507
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-10-01 17:26:10 +0200

    neue Data-Objekt format für den Vokabeltrainer angefangen

---
+++ script.js
@@ -0,0 +1,19 @@
+export let vokabeln = {
+    'Unit 1 - Essen & Trinken': [
+        { wort: 'Brot', fremdsprache: 'Bread' },
+        { wort: 'Käse', fremdsprache: 'Cheese' },
+        { wort: 'Milch', fremdsprache: 'Milk' },
+        { wort: 'Wasser', fremdsprache: 'Water' },
+        { wort: 'Fleisch', fremdsprache: 'Meat' },
+        { wort: 'Obst', fremdsprache: 'Fruit' }
+    ],
+
+    'Unit 2 - Zuhause': [
+        { wort: 'Tisch', fremdsprache: 'Table' },
+        { wort: 'Stuhl', fremdsprache: 'Chair' },
+        { wort: 'Bett', fremdsprache: 'Bed' },
+        { wort: 'Fenster', fremdsprache: 'Window' },
+        { wort: 'Tür', fremdsprache: 'Door' },
+        { wort: 'Küche', fremdsprache: 'Kitchen' }
+    ],
+};
